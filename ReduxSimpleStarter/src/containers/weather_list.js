4fa3ca28a497540a10aa52c3e275456c8b479e57
Saurabh Chaudhary <sauchau0@publicisgroupe.net>
2018-07-14 13:56:02 +0530

    code changes

--- ReduxSimpleStarter/src/containers/weather_list.js
+++ ReduxSimpleStarter/src/containers/weather_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import {Sparklines, SparklinesLine} from 'react-sparklines';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 
 class WeatherList extends Component {
@@ -9,15 +9,33 @@ class WeatherList extends Component {
     renderWeather(cityData) {
         console.log('data', cityData);
         const name = cityData.city.name;
+        const temp = cityData.list.map(weather => weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+        console.log(temp);
         return (
             <tr key={name}>
                 <td>
                     {name}
                 </td>
+                <td>
+                    <Sparklines height={120} width={180} data={temp}>
+                        <SparklinesLine color="red"/>
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines height={120} width={180} data={pressure}>
+                        <SparklinesLine color="red"/>
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines height={120} width={180} data={humidity}>
+                        <SparklinesLine color="red"/>
+                    </Sparklines>
+                </td>
             </tr>
 
-        );
-        
+        ); 
     }
     
     render() {
